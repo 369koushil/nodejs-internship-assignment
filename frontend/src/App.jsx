10ab8680774bc50e5ebd7eea55914fd00367dc72
369koushil <koushil463@gmail.com>
2024-12-07 21:33:04 +0530

    adding frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,8 +10,8 @@ const HomePage = () => {
       <h1 className="text-3xl font-bold mb-6 text-blue-600">School Management App</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full max-w-md">
        
-        <Button color={"bg-red"} content={"Git Source Code Repo"} path={"https://github.com/369koushil/nodejs-internship-assignment"}></Button>
-        <Button color={"bg-green"} content={"Postman Collection"} path={"https://web.postman.co/workspace/My-Workspace~4de7ad93-3c6e-45af-88fd-d573d7e896e0/collection/37753169-b81acbf0-6eea-41bb-a3c3-208373a2b951"}></Button>
+        <Button color={"bg-red-500"} hover={"bg-red-700"} content={"Git Source Code Repo"} path={"https://github.com/369koushil/nodejs-internship-assignment"}></Button>
+        <Button color={"bg-green-500"} hover={"bg-green-700"} content={"Postman Collection"} path={"https://web.postman.co/workspace/My-Workspace~4de7ad93-3c6e-45af-88fd-d573d7e896e0/collection/37753169-b81acbf0-6eea-41bb-a3c3-208373a2b951"}></Button>
         <Link to="/add-school">
           <button className="w-full py-2 px-4 bg-purple-500 text-white rounded shadow hover:bg-purple-600">
             Add School
--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Button = ({content,path,color}) => {
+const Button = ({content,path,color,hover}) => {
   return (
     <a href={`${path}`} download>
-          <button className={`w-full py-2 px-4  text-white ${color}-500 rounded shadow hover:${color}-600`}>
+          <button className={`w-full py-2 px-4  text-white ${color} rounded shadow hover:${hover}`}>
             {content}
           </button>
         </a>
